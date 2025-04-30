9ed5708015bf59827d4df1c7ac6727b04b5ca21f
GRGHSS <rafaell.ghazaryan@gmail.com>
2025-04-30 19:10:39 +0400

    task 4

---
+++ task4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int size = 0;
+  printf("enter size of array\n");
+  scanf("%d", &size);
+  printf("now enter %d integers\n", size);
+  int* p = (int*)calloc(size, sizeof(int));
+  for(int i = 0; i < size; i++){
+    scanf("%d", &p[i]);
+  }
+  int size1 = 0;
+  printf("how many elements do you wanna add?\n");
+  scanf("%d", &size1);
+  size1 = size1 + size;
+  printf("enter %d integers\n", size1 - size);
+  for(int i = size; i < size1; i++){
+    scanf("%d", &p[i]);
+  }
+  puts("your array:");
+  for(int i = 0; i < size1; i++){
+    printf("%d ", p[i]);
+  }
+  free(p);
+  return 0;
+}
