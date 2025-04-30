70365e31ed0586f2a0fa63b96fc79125003a79a9
GRGHSS <rafaell.ghazaryan@gmail.com>
2025-04-30 18:40:28 +0400

    first task

---
+++ task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int size = 0;
+  printf("enter size of array\n");
+  scanf("%d",&size);
+  int* p = calloc(size, sizeof(int));
+  printf(" now enter %d integers\n", size);
+  for(int i = 0; i < size; ++i){
+    scanf("%d", &p[i]);
+  }
+printf("your array is: ");
+  for(int i = 0; i < size; ++i){
+  printf("%d ", p[i]);
+}
+printf("\n");
+free(p);
+return 0;
+}
