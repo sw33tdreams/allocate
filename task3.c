f48509692d422e6730ef1fa8cdd5d29ced90fd92
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-05-04 12:28:34 +0400

    issue fix

--- task3.c
+++ task3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   printf("enter number of numbers\n");
   int size = 0;
   scanf("%d", &size);
   float* p = (float*)calloc(size, sizeof(float));
+  if(p == 0) exit(-1);
   printf("enter %d floats\n", size);
   for(int i = 0; i < size; i++){
     scanf("%f", &p[i]);
